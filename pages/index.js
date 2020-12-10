ce65f6f0d2a2f56b787199a36f5b0d4b7300091b
Balohé <balohe37pro@gmail.com>
2020-12-10 12:42:51 +0100

    Test the Polaris import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import { TextStyle } from '@shopify/polaris';
+
 const Index = () => (
     <div>
-      <p>Sample app using React and Next.js</p>
+      <p>Vegesims store</p>
+      <p>
+  New URL that visitors should be forwarded to. If you want your store’s
+  homepage, enter <TextStyle variation="code"> / </TextStyle> (a forward slash).
+     </p>
+      <TextStyle variation="subdued">No supplier listed</TextStyle>
+      <TextStyle variation="strong">No supplier listed</TextStyle>
+      <TextStyle variation="positive">No supplier listed</TextStyle>
     </div>
   );
   
